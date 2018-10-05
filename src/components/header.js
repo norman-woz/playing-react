1ceea4d4c62c2f6c0d6e354e4f610457da51cd35
Norman Wozniak <inormi@Norman-says-Hello.local>
2018-10-05 16:48:02 -0400

    First commit baby

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,19 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import logo from '../images/logo-drone.svg';
+import './Header.css'
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <div>
+    <div className="Header">
+      <div className="HeaderGroup">
+        <Link to="/"><img src={logo} width="60" /></Link>
+        <Link to="/boston">Boston</Link>
+        <Link to="/national-parks">National Parks</Link>
+        <Link to="/request">Request</Link>
+        <Link to="/buy"><button>Buy</button></Link>
+      </div>
     </div>
   </div>
 )
-
 export default Header
